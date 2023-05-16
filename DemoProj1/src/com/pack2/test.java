1f60f7e18d57858c647ecbcad957ef459613bb82
Dev1 <Dev1@gmail.com>
2023-05-16 15:22:12 +0530

    Dev1 modified test.java

--- DemoProj1/src/com/pack2/test.java
+++ DemoProj1/src/com/pack2/test.java
@@ -1,5 +1,14 @@
 package com.pack2;
 
-public class test {
+import com.pack1.Utility;
+
+public class test
+{
+	public static void main(String[] args)
+	{
+		Utility u=new Utility();
+		System.out.println(u.getSysDate());
+		
+	}
 
 }
